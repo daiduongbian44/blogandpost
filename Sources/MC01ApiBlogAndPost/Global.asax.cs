540b867fb08b6be5fe569a3028971e58bdab7dc2
Mạnh Nguyễn <nguyenquangmanh9099@gmail.com>
2019-04-21 09:13:07 +0700

    Fix bug

--- Sources/MC01ApiBlogAndPost/Global.asax.cs
+++ Sources/MC01ApiBlogAndPost/Global.asax.cs
@@ -19,5 +19,16 @@ namespace MC01ApiBlogAndPost
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_BeginRequest()
+        {
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
     }
 }
